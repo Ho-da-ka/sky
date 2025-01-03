3c05e4d4d20702fcf04950bbbf2fcdd10920c762
eric <eric@example.com>
2025-01-03 17:37:07 +0800

    fix(sky-server): 修改数据库密码并优化密码验证逻辑
    - 修改 application-dev.yml 中的数据库密码为 284100- 在 EmployeeServiceImpl 中实现密码的 MD5 加密验证

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -38,9 +38,9 @@ public class EmployeeServiceImpl implements EmployeeService {
             throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);
         }
 
-        //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
-        if (!password.equals(employee.getPassword())) {
+        //密码比对,需要进行md5加密，然后再进行比对
+        String passwordMD5 = DigestUtils.md5DigestAsHex(password.getBytes());
+        if (!passwordMD5.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
         }
