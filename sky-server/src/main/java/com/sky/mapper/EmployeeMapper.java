1844f432cacf3b0385e7fa19adad8066d60f242b
eric <eric@example.com>
2025-01-04 14:57:28 +0800

    feat(employee): 添加员工分页查询功能
    - 在 EmployeeController 中添加分页查询接口
    - 在 EmployeeService 接口中定义分页查询方法
    - 实现 EmployeeServiceImpl 中的分页查询逻辑
    - 在 EmployeeMapper 中添加分页查询的 SQL 方法
    - 更新 WebMvcConfiguration 配置类，扩展消息转换器

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -24,5 +26,11 @@ public interface EmployeeMapper {
      */
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
-
+    /**
+     * 根据姓名分页查询员工
+     *
+     * @param employeePageQueryDTO
+     * @return Page<Employee>
+     */
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
